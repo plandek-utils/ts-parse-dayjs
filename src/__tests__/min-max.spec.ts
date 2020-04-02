196147cbacfd32894e74385bcffeebcde6e73dd1
Eduardo Turiño <eturino@eturino.com>
2020-04-02 13:10:56 +0100

    fix: fixing type issue with minDayjs() and maxDayjs()

--- src/__tests__/min-max.spec.ts
+++ src/__tests__/min-max.spec.ts
@@ -1,9 +1,10 @@
-import { minDayjs, maxDayjs, parseDayjsOrError } from "..";
+import { minDayjs, maxDayjs, parseDayjsOrError, Dayjs } from "..";
 
 const d1 = parseDayjsOrError("2019-01-01");
 const d2 = parseDayjsOrError("2019-02-02");
 const d3 = parseDayjsOrError("2019-03-03");
 const d4 = parseDayjsOrError("2019-04-04");
+const list: Dayjs[] = [d3, d4, d1, d2];
 
 describe("minDayjs([d1, d2, d3])", () => {
   it("returns null with empty array", () => {
@@ -12,6 +13,7 @@ describe("minDayjs([d1, d2, d3])", () => {
 
   it("returns the earliest with non empty array", () => {
     expect(minDayjs([d3, d4, d1, d2])).toBe(d1);
+    expect(minDayjs(list)).toBe(d1);
   });
 });
 
@@ -22,5 +24,6 @@ describe("maxDayjs([d1, d2, d3])", () => {
 
   it("returns the latest with non empty array", () => {
     expect(maxDayjs([d3, d4, d1, d2])).toBe(d4);
+    expect(maxDayjs(list)).toBe(d4);
   });
 });
