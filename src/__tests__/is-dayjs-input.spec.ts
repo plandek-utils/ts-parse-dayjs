0a3d7c3dc59664279635901c98aadca296a70184
Eduardo Turiño <eturino@eturino.com>
2020-06-01 20:25:23 +0100

    feat: add `isStrictDayjsInput()` and `isDayjsInput()`

---
+++ src/__tests__/is-dayjs-input.spec.ts
@@ -0,0 +1,78 @@
+import { isStrictDayjsInput, isDayjsInput } from "../is-dayjs-input";
+import { parseDayjsOrError } from "../parse";
+
+describe("isStrictDayjsInput", () => {
+  it("isStrictDayjsInput(null) => false", () => {
+    expect(isStrictDayjsInput(null)).toBeFalsy();
+  });
+  it("isStrictDayjsInput(undefined) => false", () => {
+    expect(isStrictDayjsInput(undefined)).toBeFalsy();
+  });
+  it("isStrictDayjsInput([]) => false", () => {
+    expect(isStrictDayjsInput([])).toBeFalsy();
+  });
+  it("isStrictDayjsInput({}) => false", () => {
+    expect(isStrictDayjsInput({})).toBeFalsy();
+  });
+  it("isStrictDayjsInput(true) => false", () => {
+    expect(isStrictDayjsInput(true)).toBeFalsy();
+  });
+  it("isStrictDayjsInput(false) => false", () => {
+    expect(isStrictDayjsInput(false)).toBeFalsy();
+  });
+  it("isStrictDayjsInput(() => {})) => false", () => {
+    // tslint:disable-next-line:no-empty
+    expect(isStrictDayjsInput(() => {})).toBeFalsy();
+  });
+
+  it("isStrictDayjsInput(new Date())) => true", () => {
+    expect(isStrictDayjsInput(new Date())).toBeTruthy();
+  });
+  it("isStrictDayjsInput(dayjsObject)) => true", () => {
+    expect(isStrictDayjsInput(parseDayjsOrError(new Date()))).toBeTruthy();
+  });
+  it("isStrictDayjsInput('whatever')) => true", () => {
+    expect(isStrictDayjsInput("whatever")).toBeTruthy();
+  });
+  it("isStrictDayjsInput(123)) => true", () => {
+    expect(isStrictDayjsInput(123)).toBeTruthy();
+  });
+});
+
+describe("isDayjsInput", () => {
+  it("isDayjsInput([]) => false", () => {
+    expect(isDayjsInput([])).toBeFalsy();
+  });
+  it("isDayjsInput({}) => false", () => {
+    expect(isDayjsInput({})).toBeFalsy();
+  });
+  it("isDayjsInput(true) => false", () => {
+    expect(isDayjsInput(true)).toBeFalsy();
+  });
+  it("isDayjsInput(false) => false", () => {
+    expect(isDayjsInput(false)).toBeFalsy();
+  });
+  it("isDayjsInput(() => {})) => false", () => {
+    // tslint:disable-next-line:no-empty
+    expect(isDayjsInput(() => {})).toBeFalsy();
+  });
+  it("isDayjsInput(null) => true", () => {
+    expect(isDayjsInput(null)).toBeTruthy();
+  });
+  it("isDayjsInput(undefined) => true", () => {
+    expect(isDayjsInput(undefined)).toBeTruthy();
+  });
+
+  it("isDayjsInput(new Date())) => true", () => {
+    expect(isDayjsInput(new Date())).toBeTruthy();
+  });
+  it("isDayjsInput(dayjsObject)) => true", () => {
+    expect(isDayjsInput(parseDayjsOrError(new Date()))).toBeTruthy();
+  });
+  it("isDayjsInput('whatever')) => true", () => {
+    expect(isDayjsInput("whatever")).toBeTruthy();
+  });
+  it("isDayjsInput(123)) => true", () => {
+    expect(isDayjsInput(123)).toBeTruthy();
+  });
+});
---
+++ src/is-dayjs-input.ts
@@ -0,0 +1,22 @@
+import isNil from "lodash.isnil";
+import { isDayjs } from "dayjs";
+import { isValidNumber } from "./utils";
+import { StrictDayjsInput, DayjsInput } from "./base";
+
+/**
+ * returns true if the given param is a Dayjs object, Date, number (except NaN) or string
+ *
+ * @param x
+ */
+export function isStrictDayjsInput(x: any): x is StrictDayjsInput {
+  if (isNil(x)) return false;
+  return isDayjs(x) || x instanceof Date || isValidNumber(x) || typeof x === "string";
+}
+
+/**
+ * returns true if the given param is a Dayjs object, Date, number (except NaN), string, null or undefined
+ * @param x
+ */
+export function isDayjsInput(x: any): x is DayjsInput {
+  return isNil(x) || isStrictDayjsInput(x);
+}
