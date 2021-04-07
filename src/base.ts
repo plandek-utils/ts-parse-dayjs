436069b8cac6e0f9876c00df88458750ffa5c23a
Eduardo Turiño <eturino@eturino.com>
2021-04-07 16:27:21 +0200

    feat: adds RelativeTime plugin support

--- src/base.ts
+++ src/base.ts
@@ -15,6 +15,7 @@ import "dayjs/plugin/isBetween";
 import "dayjs/plugin/isSameOrAfter";
 import "dayjs/plugin/isSameOrBefore";
 import "dayjs/plugin/minMax";
+import "dayjs/plugin/relativeTime";
 import "dayjs/plugin/utc";
 import "dayjs/plugin/weekOfYear";
 
@@ -26,6 +27,7 @@ import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
 import minMax from "dayjs/plugin/minMax";
 import utc from "dayjs/plugin/utc";
 import weekOfYear from "dayjs/plugin/weekOfYear";
+import relativeTime from "dayjs/plugin/relativeTime";
 // tslint:enable:no-duplicate-imports
 // tslint:enable:no-submodule-imports
 
@@ -35,6 +37,7 @@ dayjs.extend(isBetween);
 dayjs.extend(isSameOrAfter);
 dayjs.extend(isSameOrBefore);
 dayjs.extend(minMax);
+dayjs.extend(relativeTime);
 dayjs.extend(utc);
 dayjs.extend(weekOfYear);
 
