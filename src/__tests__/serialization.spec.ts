268b4a02fdbebd344fe27df738bf11210a208d29
eturino <eturino@eturino.com>
2024-11-29 15:19:34 +0100

    chore: adding more tests

---
+++ src/__tests__/serialization.spec.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from "vitest";
+import { TimeOverride, parseDayjsOrError } from "..";
+
+describe("serialization testing", () => {
+  it("should serialize", () => {
+    const json = JSON.stringify({ stuff: "testing" });
+    expect(json).toEqual('{"stuff":"testing"}');
+  });
+  it("serializes date range", () => {
+    const dr = {
+      from: parseDayjsOrError("2021-01-01", { time: TimeOverride.StartOfDay }),
+      to: parseDayjsOrError("2021-01-02", { time: TimeOverride.EndOfDay }),
+    };
+    const expected = '{"from":"2021-01-01T00:00:00.000Z","to":"2021-01-02T23:59:59.999Z"}';
+    const actual = JSON.stringify(dr);
+    expect(actual).toEqual(expected);
+  });
+});
